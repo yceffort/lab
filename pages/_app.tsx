7740e377aa62c711e3d27c41793e4d6866101b39
yceffort <yceffort@gmail.com>
2022-06-23 22:40:36 +0900

    :lipstick: add reveal.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/black.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
